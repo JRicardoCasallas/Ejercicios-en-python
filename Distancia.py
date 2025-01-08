c7222caf560aba5f7b222ae4016051adb3c57881
Jose Ricado Casallas <josericardocasasllas11@gmail.com>
2025-01-08 00:38:31 -0500

    Ejercicios en python

---
+++ Distancia.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8
+# Decoracion del algoritmo
+print("---------------------------------------------")
+print("ejercicio 1 calcular Distacia")
+print("---------------------------------------------")
+
+#Entradas
+print("ingrese la velocidad y el tiempo de la unidad movil")
+
+#convirtiendo entrada a entero
+
+v = float(input("velocidad: "))
+t = int(input("tiempo: "))
+
+D = v*t
+
+#salida (D)
+print(D)
