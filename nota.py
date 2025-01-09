c774357e3035cb0cc066922f0083e9c75e55c8dc
Jose Ricado Casallas <josericardocasasllas11@gmail.com>
2025-01-09 18:11:56 -0500

    Ejercicio5: NÚMERO DE MICRO DISCOS 3.5 NECESARIOS

--- nota.py
+++ nota.py
@@ -19,7 +19,7 @@ print( P )"""
 
  #-*- coding: utf-8 -*- 
 #Decoración: Nombre del Algoritmo 
-print("-------------------------------------------------------") 
+"""print("-------------------------------------------------------") 
 print("Ejercicio3: PUNTAJE FINAL.") 
 print("-------------------------------------------------------") 
 #Entradas 
@@ -36,4 +36,68 @@ PRB = RB*0
  
 PF = PCR + PRI + PRB 
 #Salida 
-print("El puntaje total es:", PF) 
+print("El puntaje total es:", PF) """
+
+
+
+
+
+
+
+# -*- coding: utf-8 -*- 
+
+#Decoración: Nombre del Algoritmo 
+"""print("-------------------------------------------------------") 
+print("Ejercicio4: PUNTAJE TOTAL DE PARTIDOS.") 
+print("-------------------------------------------------------") 
+ 
+#Entradas 
+print("Ingrese número de partidos ganados") 
+PG = int( input()) 
+print("Ingrese número de partidos empatados") 
+PE = int( input()) 
+print("Ingrese número de partidos perdidos") 
+PP = int( input()) 
+ 
+#Proceso 
+PPG = PG*3 
+PPE = PE*1 
+PPP = PP*0 
+ 
+PF = PPG + PPE + PPP 
+ 
+#Salida 
+print("\nSALIDA: ") 
+print("-------------------------------------------------------") 
+print("Puntaje Final: ", PF)"""
+
+
+
+
+
+
+
+ # -*- coding: utf-8 -*- 
+import math #librería necesaria para usar funciones Matemáticas 
+#en este caso math.ceil(), que redondea un numero al Entero      superior 
+ 
+#Decoración: Nombre del Algoritmo 
+print("-------------------------------------------------------") 
+print("Ejercicio5: NÚMERO DE MICRO DISCOS 3.5 NECESARIOS") 
+print("-------------------------------------------------------") 
+ 
+#Entradas 
+print("Ingrese GB: ") 
+GB = float( input()) 
+ 
+#Proceso 
+MG = GB*1024 
+MD = MG/1.44 
+ 
+#Salida 
+print("\nSALIDA: ") 
+print("-------------------------------------------------------") 
+print(MD) 
+#En caso de Decimal Aproximar al siguiente entero 
+#Ya que la parte decimal debe ser almacenada en otro DISCO 3.5 
+print("Numero de Discos necesarios: ", math.ceil(MD)) 
